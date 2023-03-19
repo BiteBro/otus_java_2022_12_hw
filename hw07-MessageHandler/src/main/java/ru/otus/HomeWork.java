a9bd880ff152f1e84eb4783796d29f138ecef874
Rozhnevskiy Alexey <ralewww@gmail.com>
2023-03-20 00:57:48 +0300

    hw07-MessageHandler is done!

--- hw07-MessageHandler/src/main/java/ru/otus/HomeWork.java
+++ hw07-MessageHandler/src/main/java/ru/otus/HomeWork.java
@@ -1,12 +1,24 @@
 package ru.otus;
 
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.LoggerProcessor;
+import ru.otus.processor.ProcessorConcatFields;
+import ru.otus.processor.ProcessorUpperField10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HomeWork {
 
     /*
      Реализовать to do:
-       1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
-       2. Сделать процессор, который поменяет местами значения field11 и field12
-       3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)
+       1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage) !!!
+       2. Сделать процессор, который поменяет местами значения field11 и field12  !!!
+       3. Сделать процессор, который будет выбрасывать исключение в четную секунду (сделайте тест с гарантированным результатом)  !!!
              Секунда должна определяьться во время выполнения.
              Тест - важная часть задания
              Обязательно посмотрите пример к паттерну Мементо!
@@ -16,6 +28,35 @@ public class HomeWork {
      */
 
     public static void main(String[] args) {
+        var processors = List.of(new ProcessorConcatFields(),
+                new LoggerProcessor(new ProcessorUpperField10()));
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {
+        });
+        var listenerPrinter = new ListenerPrinterConsole();
+        complexProcessor.addListener(listenerPrinter);
+
+        List<String> list = new ArrayList<>();
+        list.add("sdvsd");
+
+        ObjectForMessage objectForMessage = new ObjectForMessage();
+        objectForMessage.setData(list);
+
+        var message = new Message.Builder(1L)
+                .field1("field1")
+                .field3("field3")
+                .field5("field5")
+                .field8("field8")
+                .field6("field6")
+                .field10("field10")
+                .field13(objectForMessage)
+                .build();
+
+        var result = complexProcessor.handle(message);
+        System.out.println("result:" + result);
+
+        complexProcessor.removeListener(listenerPrinter);
+
         /*
            по аналогии с Demo.class
            из элеменов "to do" создать new ComplexProcessor и обработать сообщение
