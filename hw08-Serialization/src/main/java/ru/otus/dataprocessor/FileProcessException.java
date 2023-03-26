5169d2760a19bc5561927a8dcbe7c7a70f1ae15c
Rozhnevskiy Alexey <ralewww@gmail.com>
2023-03-26 07:26:21 +0300

    Homework Serialization is done.

--- hw08-Serialization/src/main/java/ru/otus/dataprocessor/FileProcessException.java
+++ hw08-Serialization/src/main/java/ru/otus/dataprocessor/FileProcessException.java
@@ -4,7 +4,6 @@ public class FileProcessException extends RuntimeException {
     public FileProcessException(Exception ex) {
         super(ex);
     }
-
     public FileProcessException(String msg) {
         super(msg);
     }
--- hw08-Serialization/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-Serialization/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -2,14 +2,17 @@ package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
-    @Override
+    @Override   //группирует выходящий список по name, при этом суммирует поля value
     public Map<String, Double> process(List<Measurement> data) {
-        //группирует выходящий список по name, при этом суммирует поля value
-        return null;
+
+        return data.stream().sorted(Comparator.comparing(Measurement::getName))
+                .collect(Collectors.groupingBy(
+                        Measurement::getName, LinkedHashMap::new,
+                        Collectors.summingDouble(Measurement::getValue)));
     }
 }
