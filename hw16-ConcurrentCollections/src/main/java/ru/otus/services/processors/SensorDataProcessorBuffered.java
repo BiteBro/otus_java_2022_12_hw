7e8714a6287cd6e9663f7b8068cc23be3b47a9bc
Rozhnevskiy Alexey <ralewww@gmail.com>
2023-06-29 00:38:54 +0300

    Homework hw16-ConcurrentCollections is done

--- hw16-ConcurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-ConcurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,30 +6,39 @@ import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 import ru.otus.lib.SensorDataBufferedWriter;
 
-// Этот класс нужно реализовать
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+    private final BlockingQueue<SensorData> bufferedData;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.bufferedData = new PriorityBlockingQueue<>(bufferSize,
+                Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
+        if (bufferedData.size() >= bufferSize) {
             flush();
         }
-    */
+        bufferedData.add(data);
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            if (!bufferedData.isEmpty()) {
+                List<SensorData> dataList = new ArrayList<>();
+                bufferedData.drainTo(dataList, bufferSize);
+                writer.writeBufferedData(dataList);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
